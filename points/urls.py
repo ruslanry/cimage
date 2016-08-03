96c89c3c5adf2d1a20e3a555a5c5cb22fc2daf3b
ruslanry <ruslan.ry@gmail.com>
2016-08-03 17:49:50 +0500

    add main map, add json

--- points/urls.py
+++ points/urls.py
@@ -5,11 +5,15 @@ from . import views
 app_name = 'points'
 
 urlpatterns = [
-    # ex: /polls/
+    # ex: /points/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /points/5/
     url(r'^(?P<point_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/collect/
+    # ex: /points/collect/
     url(r'^collect/$', views.collect, name='collect'),
+    # ex: /points/maps/
+    url(r'^maps/$', views.point_maps, name='point_maps'),
+    # ex: /points/mapsjson/
+    url(r'^maps/json.dat$', views.all_point_json, name='point_json'),
 ]
 
--- points/views.py
+++ points/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from .models import Point
+import json
+from decimal import Decimal
 
 import wikicollector as wc
 
@@ -9,6 +11,21 @@ def index(request):
     points_list = Point.objects.order_by('point_name')
     context = {'points_list': points_list}
     return render(request, 'points/index.html', context)
+
+def point_maps(request):
+    points_list = Point.objects.order_by('point_name')
+    context = {'points_list': points_list}
+    return render(request, 'points/pointmaps.html', context)
+
+def default(obj):
+    if isinstance(obj, Decimal):
+        return str(obj)
+    raise TypeError
+
+def all_point_json(request):
+    result = Point.objects.values()
+    list_result = [entry for entry in result]
+    return HttpResponse(json.dumps(list_result, default=default), content_type='application/json')
 
 def detail(request, point_id):
     point = get_object_or_404(Point, pk=point_id)
